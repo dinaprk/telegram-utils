86e8be6cf046ccd1e000e490f2483268206b7709
Динаzavr <dinaprk@cumallover.me>
2024-02-03 12:55:42 +0300

    Fix evenv type

--- pfp_dump.py
+++ pfp_dump.py
@@ -1,14 +1,15 @@
-from telethon.sync import TelegramClient, events
+from telethon.sync import TelegramClient
 from telethon.tl.types import Photo
+from telethon.events import NewMessage
 
 from auth import API_ID, API_HASH, SESSION
 
 with TelegramClient(SESSION, API_ID, API_HASH) as client:
-    @client.on(events.NewMessage(pattern="/pfp.*"))
-    async def cmd_pfp(event):
+    @client.on(NewMessage(pattern="/pfp.*"))
+    async def cmd_pfp(event: NewMessage.Event):
         message_words = event.text.split()
-        target: str = message_words[1]
-        target = event.chat_id if target == "." else target
+        target = message_words[1]
+        target = event.chat_id if message_words == "." else target
         try:
             params = {"limit": int(message_words[2])}
         except (IndexError, ValueError):
